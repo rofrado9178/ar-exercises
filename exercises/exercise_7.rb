733b0da33818fe9b41605a9430c41558a3afa836
Fransiskus <aldzaidea@gmail.com>
2022-03-08 01:39:48 +0000

    add excercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,17 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+# puts "choose your store name"
+# @pickstore = gets.chomp
+# puts @pickstore
+
+#  @store1.employees.create(first_name:"first",last_name:"Jack",hourly_rate:50)
+
+x = Store.new(name:"NewOne", annual_revenue: 1000, mens_apparel: false, womens_apparel: false)
+y = Store.new(name:"storeY", annual_revenue: 100, mens_apparel: false, womens_apparel: false)
+z =  Store.new(name:"storeZZZZ", annual_revenue: 100, mens_apparel: false, womens_apparel: false)
+
+x.save
+y.save
+z.save
+puts x.errors.inspect
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
-has_many :employees
+  has_many :employees
+  validate :mens_or_womens_store
+  validates :name, presence: true
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+  def mens_or_womens_store
+    if !mens_apparel  && !womens_apparel 
+      errors.add(:mens_apparel,"must be choose  mens or womens apparel or both")
+      return false
+    end
+  end
 end
+
