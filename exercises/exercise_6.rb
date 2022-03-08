7415a16be7cb0de1438b5efdea23eab73066dc43
Fransiskus <aldzaidea@gmail.com>
2022-03-08 00:19:43 +0000

    add exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,9 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+# Your code goes here .
+
+@store1.employees.create(first_name:"Fransiskus", last_name:"Aldo",hourly_rate:1000)
+@store1.employees.create(first_name:"Shevina", last_name:"Valentina",hourly_rate:1000)
+@store2.employees.create(first_name:"Isabela", last_name:"Valdo",hourly_rate:1000000)
+
